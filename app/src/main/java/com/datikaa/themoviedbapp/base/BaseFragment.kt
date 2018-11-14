e444d2c0ca577478ac0b2bd3c67a85e637ec7889
datikaa <daniel.dattler@gmail.com>
2018-11-14 22:48:25 +0100

    Initial commit

---
+++ app/src/main/java/com/datikaa/themoviedbapp/base/BaseFragment.kt
@@ -0,0 +1,13 @@
+package com.datikaa.themoviedbapp.base
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+abstract class BaseFragment : Fragment() {
+    // yet to be utilized
+}
---
+++ app/src/main/java/com/datikaa/themoviedbapp/fragment/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.datikaa.themoviedbapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.datikaa.themoviedbapp.R
+import com.datikaa.themoviedbapp.base.BaseFragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class HomeFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BaseFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+        @JvmStatic
+        fun newInstance() = HomeFragment()
+    }
+}
