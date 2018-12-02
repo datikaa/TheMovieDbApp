157fef6d21e0c6816c27c466c3fb539289e5207b
datiKaa <daniel.dattler@gmail.com>
2018-12-02 12:21:33 +0100

    Added extension to Int for easy DP/PX converions

--- app/src/main/java/com/datikaa/themoviedbapp/common/Extensions.kt
+++ app/src/main/java/com/datikaa/themoviedbapp/common/Extensions.kt
@@ -1,9 +1,11 @@
 package com.datikaa.themoviedbapp.common
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.LinearLayoutManager
 
 /**
  * Convenience extension method for layout inflation
@@ -11,3 +13,13 @@ import androidx.annotation.LayoutRes
 fun ViewGroup.inflate(@LayoutRes layoutId: Int, attachToRoot: Boolean = false) : View {
     return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 }
+
+/**
+ * convert Int to Dp
+ */
+fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+/**
+ * convert Int to Px
+ */
+fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
