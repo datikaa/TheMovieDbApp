e744a8e291cfb31322578aa595892864d95a5e0d
datiKaa <daniel.dattler@gmail.com>
2019-08-27 10:36:53 +0200

    refactor api key accessing

--- app/src/main/java/com/datikaa/themoviedbapp/Constants.kt
+++ app/src/main/java/com/datikaa/themoviedbapp/Constants.kt
@@ -2,5 +2,4 @@ package com.datikaa.themoviedbapp
 
 const val PicassoBaseUrl: String = "https://image.tmdb.org/t/p"
 const val PicSizeW500: String = "/w500"
-val tmdbApiKey: String
-    get() = BuildConfig.TMDB_API_KEY
+const val tmdbApiKey: String = BuildConfig.TMDB_API_KEY
