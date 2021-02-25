418aa2db29b320496c15ac8889260d7da595d69a
daniel.dattler <daniel.dattler@bcsconsult.hu>
2021-02-25 13:25:18 +0100

    Using nice Insets for UI

--- app/src/main/java/com/datikaa/themoviedbapp/MainActivity.kt
+++ app/src/main/java/com/datikaa/themoviedbapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.HiltViewModelFactory
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,22 +12,27 @@ import androidx.navigation.compose.rememberNavController
 import com.datikaa.themoviedbapp.ui.compose.HomeComposable
 import com.datikaa.themoviedbapp.ui.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import dev.chrisbanes.accompanist.insets.ExperimentalAnimatedInsets
+import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @ExperimentalAnimatedInsets
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val navController = rememberNavController()
-            NavHost(
-                navController = navController,
-                startDestination = "home"
-            ) {
-                composable("home") {
-                    HiltViewModelFactory(LocalContext.current, it)
-                    HomeComposable(navController, it.hiltViewModel())
+            ProvideWindowInsets(windowInsetsAnimationsEnabled = true) {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable("home") {
+                        HiltViewModelFactory(LocalContext.current, it)
+                        HomeComposable(navController, it.hiltViewModel())
+                    }
                 }
             }
         }
--- app/src/main/java/com/datikaa/themoviedbapp/ui/compose/HomeComposable.kt
+++ app/src/main/java/com/datikaa/themoviedbapp/ui/compose/HomeComposable.kt
@@ -7,17 +7,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -32,30 +33,43 @@ import com.datikaa.themoviedbapp.R
 import com.datikaa.themoviedbapp.api.model.UpcomingMovie
 import com.datikaa.themoviedbapp.ui.home.HomeViewModel
 import dev.chrisbanes.accompanist.coil.CoilImage
+import dev.chrisbanes.accompanist.insets.LocalWindowInsets
+import dev.chrisbanes.accompanist.insets.navigationBarsPadding
+import dev.chrisbanes.accompanist.insets.statusBarsPadding
+import dev.chrisbanes.accompanist.insets.toPaddingValues
 
 
 @Composable
 fun HomeComposable(navController: NavController, viewModel: HomeViewModel) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Title") }) },
-        content = {
-            HomeContentComposable(
-                upcomingMovies = viewModel.upcomingMovies.collectAsState(
-                    emptyList()
-                )
-            )
-        }
-    )
-}
+    val itemsState = viewModel.upcomingMovies.collectAsState(emptyList())
 
-@Composable
-fun HomeContentComposable(upcomingMovies: State<List<UpcomingMovie>>) {
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        items(upcomingMovies.value) {
-            HomeItemComposable(upcomingMovie = it)
+    Surface {
+        Box(Modifier.fillMaxSize()) {
+            var topAppBarSize by remember { mutableStateOf(0) }
+
+            LazyColumn(
+                contentPadding = LocalWindowInsets.current.systemBars.toPaddingValues(
+                    top = false,
+                    additionalTop = with(LocalDensity.current) { topAppBarSize.toDp() + 8.dp },
+                    additionalBottom = 8.dp,
+                    additionalStart = 8.dp,
+                    additionalEnd = 8.dp,
+                ),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                items(itemsState.value) {
+                    HomeItemComposable(upcomingMovie = it)
+                }
+            }
+            InsetAwareTopAppBar(
+                title = { Text("Title") },
+                backgroundColor = MaterialTheme.colors.surface.copy(alpha = 0.9f),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    // We use onSizeChanged to track the app bar height, and update
+                    // our state above
+                    .onSizeChanged { topAppBarSize = it.height }
+            )
         }
     }
 }
@@ -104,5 +118,38 @@ fun HomeItemComposable(upcomingMovie: UpcomingMovie) {
                     }
             )
         }
+    }
+}
+
+/**
+ * A wrapper around [TopAppBar] which uses [Modifier.statusBarsPadding] to shift the app bar's
+ * contents down, but still draws the background behind the status bar too.
+ */
+@Composable
+fun InsetAwareTopAppBar(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable (() -> Unit)? = null,
+    actions: @Composable RowScope.() -> Unit = {},
+    backgroundColor: Color = MaterialTheme.colors.primarySurface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    elevation: Dp = 4.dp
+) {
+    Surface(
+        color = backgroundColor,
+        elevation = elevation,
+        modifier = modifier
+    ) {
+        TopAppBar(
+            title = title,
+            navigationIcon = navigationIcon,
+            actions = actions,
+            backgroundColor = Color.Transparent,
+            contentColor = contentColor,
+            elevation = 0.dp,
+            modifier = Modifier
+                .statusBarsPadding()
+                .navigationBarsPadding(bottom = false)
+        )
     }
 }
